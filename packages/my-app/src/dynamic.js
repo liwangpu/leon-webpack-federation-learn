4e5a852e397c4f29d58e5487e0587629c608555e
蒲立望60263 <liwang.pu@going-link.com>
2022-11-17 11:34:39 +0800

    添加favicon

--- packages/my-app/src/dynamic.js
+++ packages/my-app/src/dynamic.js
@@ -1,11 +1,4 @@
 // import { isEqual } from 'lodash';
-import Uilt from 'my-lib/Util';
+import { add } from 'my-lib/Util';
 
-// console.log(`Uilt:`, isEqual(1, 2));
-
-Uilt();
-
-// import('my-lib/Util').then(m => {
-//   m.default();
-// });
-
+add();
--- packages/my-lib/src/util.js
+++ packages/my-lib/src/util.js
@@ -1,5 +1,5 @@
 // import { isEqual } from 'lodash';
 
-export default function add() {
+export function add() {
   console.log(`add function work!`, true);
 }
